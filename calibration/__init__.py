b2c180f03815c30366c8959155e08c0c2ca680d0
brunoolmedo22-Paraguay <bruno.olmedo22@gmail.com>
2026-07-20 23:12:19 -0300

    Delete calibration directory

--- calibration/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Ferramentas reprodutíveis de digitalização, identificação e validação.
-
-A aplicação importa apenas constantes e descrições metodológicas; a otimização
-não é executada durante o uso normal da interface.
-"""
